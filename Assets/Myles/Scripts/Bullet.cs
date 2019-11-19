1c5431b77160d609f3339d315302874fe40e0cb5
Unknown <mylesd2@ferris.edu>
2019-11-19 16:41:40 -0500

    Followed Demo

---
+++ Assets/Myles/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Myles
+{
+    public class Bullet : MonoBehaviour
+    {
+
+        public float speed = 10;
+        public float lifespan = 3;
+
+        float age = 0;
+
+        Vector3 velocity = Vector3.zero;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            velocity = transform.right * speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            age += Time.deltaTime;
+            if (age >= lifespan) Destroy(gameObject);
+
+
+
+            transform.position += velocity * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Myles/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Myles
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public bool useMouseForAiming;
+        public float speed = 5;
+
+        Camera cam;
+
+        void Start()
+        {
+            cam = GameObject.FindObjectOfType<Camera>();
+
+        }
+
+
+        void Update()
+        {
+            Move();
+
+            if(useMouseForAiming) RotateWithMouse();
+            else RotateWithAnalogStick();
+        }
+
+        private void RotateWithMouse()
+        {
+           if (cam == null) {
+
+            Debug.LogError("There's no camera to do a raycast from...");
+            return;
+            }
+
+             Plane plane = new Plane(Vector3.up, transform.position);
+
+             Ray ray = camScreenPointToRay(Input.mousePosition);
+             if(plane.Raycast(ray, out float dis)) {
+             Vector3 mousePos = Ray.GetPoint(dis);
+
+              Vector3 vectorToMousePos = mousePos - transform.position;
+
+    float radians = Mathf.Atan2(vectorToMousePos.z, vectorToMousePosition);
+    float degress = radians * 180 / Mathf.PI;
+    transform.eulerAngles = new Vector3(0, -degrees, 0);
+           }
+        }
+
+
+        private void RotateWithAnalogStick()
+        {
+            float h = Input.GetAxis("Horizontal2");
+            float v = Input.GetAxis("Vertical2");
+
+            //print($"horizontal input: {h}  vertical input: {v}");
+
+            Vector3 dir = new Vector3(h, 0, v);
+
+            if (dir.magnitude < .5f) return;
+
+            float radians = Mathf.Atan2(v, h);
+            float degrees = radians * 180 / Mathf.PI;
+
+            transform.eulerAngles = new Vector3(0, degrees, 0);
+        }
+
+        private void Move()
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+
+            Vector3 dir = new Vector3(h, 0, v).normalized;
+            transform.position += dir * speed * Time.deltaTime;
+        }
+    }
+}
+
