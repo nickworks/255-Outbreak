f87e6016b6d295590f19ae848c8827c9143e0f1d
Jordan Wynalda <jwynalda6@gmail.com>
2019-11-26 16:16:31 -0500

    In class Push 11/26

---
+++ Assets/Wynalda/Scripts/AI/StatePursue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wynalda
+{
+    public class StatePursue : EnemyState
+    {
+        public override EnemyState Update()
+        {
+            /////////// BEHAVIOR:
+
+            Debug.Log("im pursuing...");
+
+            //move towards the player...
+            Vector3 disToPlayer = enemy.attackTarget.position - enemy.transform.position;
+            Vector3 dirToPlayer = disToPlayer.normalized;
+            enemy.velocity += dirToPlayer * enemy.acceleration * Time.deltaTime;
+
+            /////////// TRANSITIONS:
+            float disSqr = disToPlayer.sqrMagnitude;
+            //switch to IDLE
+            if(disSqr > enemy.pursueDistanceThreshold * enemy.pursueDistanceThreshold)
+            {
+                return new StateIdle();
+            }
+            //switch to ATTACK
+            if(disSqr < enemy.attackDistanceThreshold * enemy.attackDistanceThreshold)
+            {
+                return new StateAttack();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Wynalda/Scripts/AI/StateReload.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wynalda
+{
+    public class StateReload : EnemyState
+    {
+        float howLongReloadingTakes = 5;
+        float timeLeftUntilReloaded = 0;
+
+        public override void OnBegin(EnemyController enemy)
+        {
+            base.OnBegin(enemy);
+            timeLeftUntilReloaded = howLongReloadingTakes;
+        }
+
+
+        public override EnemyState Update()
+        {
+            ////////// BEHAVIOUR
+
+            Debug.Log("Im reloading...");
+
+            timeLeftUntilReloaded -= Time.deltaTime;
+
+            ////////// TRANSITION
+
+            if(timeLeftUntilReloaded <= 0)
+            {
+                return new StateIdle();
+            }
+
+            return null;
+        }
+    }
+}
