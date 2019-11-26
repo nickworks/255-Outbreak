78e4cd2ff484bfd53bd3604018a7a7e2db311c5a
Jaylen Jennings <jaylen_jennings@yahoo.com>
2019-11-26 15:34:32 -0500

    Followed 2nd Demo

--- Assets/Jennings/Scripts/Bullet.cs
+++ Assets/Jennings/Scripts/Bullet.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
-    public float speed = 10;
-    public float lifespan = 3;
+namespace Jennings {
+    public class Bullet : MonoBehaviour {
+        public float speed = 10;
+        public float lifespan = 3;
 
-    float age = 0;
+        float age = 0;
 
-    Vector3 velocity = Vector3.zero;
+        Vector3 velocity = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        velocity = transform.right * speed;
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            velocity = transform.right * speed;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        age += Time.deltaTime;
-        if (age >= lifespan) Destroy(gameObject);
-        transform.position += velocity * Time.deltaTime;
+        // Update is called once per frame
+        void Update()
+        {
+            age += Time.deltaTime;
+            if (age >= lifespan) Destroy(gameObject);
+            transform.position += velocity * Time.deltaTime;
+        }
     }
 }
