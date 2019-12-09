a6355d351f13f02c9d1b72accbd0aa98251458ad
Jordan Wynalda <jwynalda6@gmail.com>
2019-12-09 16:06:10 -0500

    WIP healthbar

---
+++ Assets/Wynalda/Scripts/HealthBAR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wynalda
+{
+    public class HealthBAR : MonoBehaviour
+    {
+        public Slider healthBar;
+        DamageTaker DamageTaker;
+
+        void Start()
+        {
+            DamageTaker = GameObject.FindGameObjectWithTag("Enemy").GetComponent<DamageTaker>();
+        }
+
+        void Update()
+        {
+            healthBar.value = DamageTaker.health;
+        }
+
+    }
+}
