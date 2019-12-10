51c9611477903bb701d3900322ae88379648d795
Nick <pattison.nick@gmail.com>
2019-12-10 02:27:54 -0500

    Added GameOver screen

---
+++ Assets/_Pattison/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public void GiveUp() {
+        Game.main.BackToMainMenu();
+    }
+    public void KeepOnTruckin() {
+        Game.GotoNextLevel();
+    }
+}
